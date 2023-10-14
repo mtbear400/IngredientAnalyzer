33dd531752f331433c32dd8dcaa58553912dd470
mtbear400 <54512620+mtbear400@users.noreply.github.com>
2023-10-14 10:43:27 -0500

    Update settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+
+# Environment variables are a secure way to store sensitive information
+# You should set these in your virtual environment or your system's environment settings
+
+# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+# EDAMAM_API_KEY = os.getenv('EDAMAM_API_KEY')
+# EDAMAM_APP_ID = os.getenv('EDAMAM_APP_ID')
+
+# Hardcoded for dev but DELETE before pushing to git
+OPENAI_API_KEY = ""
+EDAMAM_API_KEY = ""
+EDAMAM_APP_ID = ""
+
+# You can also include other configuration constants here
