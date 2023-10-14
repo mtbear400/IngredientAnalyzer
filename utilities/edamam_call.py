07d41c48efbdb5890ea736b5ea64b8722730f50e
mtbear400 <54512620+mtbear400@users.noreply.github.com>
2023-10-14 11:39:55 -0500

    added image url to edamam_call

--- utilities/edamam_call.py
+++ utilities/edamam_call.py
@@ -18,10 +18,11 @@ def get_ingredients(product_name):
         for hint in data.get('hints', []):
             food_data = hint.get('food', {})
             if 'foodContentsLabel' in food_data:
-                # When found, return the label and ingredients.
+                # When found, return the label, ingredients, and image URL.
                 product_label = food_data.get('label', '')
                 ingredients = food_data.get('foodContentsLabel', '')
-                return product_label, ingredients
+                image_url = food_data.get('image', '')  # assuming 'image' contains the image URL
+                return product_label, ingredients, image_url
         
         # If loop completes with no return, it means 'foodContentsLabel' was not found in any entry.
         print("No results found for ingredients.")
@@ -32,8 +33,8 @@ def get_ingredients(product_name):
         return None, None
 # Example usage
 product_name = "cocoa puffs"  # replace with the actual product name you're querying
-product_label, ingredients = get_ingredients(product_name)
+product_label, ingredients, image_url = get_ingredients(product_name)
 if product_label and ingredients:
-    print(f"Product: {product_label}\nIngredients: {ingredients}")
+    print(f"Product: {product_label}\nIngredients: {ingredients}\nImage: {image_url}")
 else:
     print("Could not fetch ingredients.")
