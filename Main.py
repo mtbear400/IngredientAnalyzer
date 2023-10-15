a6f858e44b0d60a039d3508c7bbb9484b98c7577
mtbear400 <54512620+mtbear400@users.noreply.github.com>
2023-10-14 21:55:11 -0500

    added streamlit ui

--- Main.py
+++ Main.py
@@ -8,12 +8,37 @@ import json
 # in terminal at this directory, run pip install -r requirements.txt
 # Run this main.py file to get streamlit working
 
+from utilities.gpt_call import GPTSession
+from utilities.message_manager import MessageManager
+from utilities.functions import function_definitions  # This assumes functions.py and functions_definitions.py are merged or handled appropriately.
+from utilities.user_input_manager import UserInputManager
 
-def format_for_markdown_lists(response):
-    # Find all markdown lists in the response
-    return re.sub(r"(\s|^)- (.+?)(\s|$)", r"\1\n- \2\n\3", response)
+def main():
+    # Initialize components
+    message_manager = MessageManager()
+    user_input_manager = UserInputManager(message_manager)
+    gpt_session = GPTSession(model="gpt-4", message_manager=message_manager, function_definitions=function_definitions)
 
+    # Main loop for chat
+    while True:
+        # Get and process user input
+        user_input, should_continue = user_input_manager.process_input()
+        if should_continue:
+            continue
 
-# Configure Secrets
-def configure_secrets():
-  openai.api_key = os.environ.get("OPENAI_API_KEY")
+        if user_input is None:  # This could happen if there's an input-related command or mode change
+            continue
+
+        # Add user message to the message manager
+        message_manager.add_message({"role": "user", "content": user_input})
+
+        # Make GPT call and handle functions
+        response_text, function_name, function_args = gpt_session.call_to_gpt()
+
+        # Here, you'd have additional logic to handle the response, possibly calling specific functions
+        # based on function_name and function_args, or just displaying the response_text to the user.
+
+        # If you're planning to end the session after a certain command, you'd check for that here.
+
+if __name__ == "__main__":
+    main()
