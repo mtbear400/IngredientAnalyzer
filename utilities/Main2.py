b4388e9c2ef81e8c22cd621ee194cbc070dce43f
mtbear400 <54512620+mtbear400@users.noreply.github.com>
2023-10-14 23:23:43 -0500

    chatbot just about working in very basic form

--- utilities/Main2.py
+++ utilities/Main2.py
@@ -1,8 +1,12 @@
+# to use in terminal: streamlit run FILEPATH streamlit run c:/Users/matth/AgentProjects/IngredientsAgent/IngredientAnalyzer/utilities/Main2.py
 import streamlit as st
 import openai
 from settings import OPENAI_API_KEY
+from functions import extract_and_get_ingredients  # Make sure to use the correct import statement for your file structure.
+
 st.title("Royal Taster")
-# to use in terminal: streamlit run FILEPATH streamlit run c:/Users/matth/AgentProjects/IngredientsAgent/IngredientAnalyzer/utilities/Main2.py
+
+# Set OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
 if "openai_model" not in st.session_state:
@@ -17,15 +21,24 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What food should I check out?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # Extract product info and get ingredients
+    product_string = extract_and_get_ingredients(prompt, OPENAI_API_KEY)
+    
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
+
+        initial_prompt = f"You are a food expert who specializes in longevity wellness. Our grocery stores are full of processed foods with long ingredients lists that the average consumer doesn't understand. You will be asked about specific food items and you will be given their ingredients. Your job is to identify unnatural and potentially harmful ingredients in a product as if the person asking you wants to cut out all the processed junk in their foods. For example, if you are familiar with ICE drinks called Sparkling Ice 'Flavored Sparkling Water', you should recognize that the product is advertised as zero sugar and low calorie but may have unnatural ingredients making them this way. You should read the ingredients list and describe the pros and cons for consuming the product. Pros could be it is helpful to getting off soda, but cons could be some of the unnatural ingredients found in the product. Your response should always consider the short term and long term health implications to whatever you are being asked. Your first consumer question is asking about the following product and its ingredients: {product_string}"
+
         for response in openai.ChatCompletion.create(
             model=st.session_state["openai_model"],
             messages=[
+                {"role": "system", "content": initial_prompt}
+            ] + [
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
